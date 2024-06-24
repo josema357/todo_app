efd54fb57dd51cf91662422421364267c6376d5b
Jose Manuel <j.ochoa317@gmail.com>
2024-06-24 11:41:47 -0500

    add: ngIf for plurality

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -19,11 +19,11 @@ export class LabsComponent {
   age = 26
   statusButton = true
   image = 'https://www.hola.com/imagenes/mascotas/20180925130054/consejos-para-cuidar-a-un-gatito-recien-nacido-cs/0-601-526/cuidardgatito-t.jpg?im=Resize=(1200)'
-  person = {
+  person = signal({
     name: 'Jose',
     age: 22,
     avatar: 'https://josema-portfolio.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile.5e678fe0.webp&w=828&q=100'
-  }
+  })
   clickHandler(){
     alert("Alerta uno")
   }
@@ -35,5 +35,16 @@ export class LabsComponent {
   keyDownHandler(event: KeyboardEvent){
     const input = event.target as HTMLInputElement;
     console.log(input.value);
+  }
+
+  changeAge(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update(prevState => {
+      return {
+        ...prevState,
+        age: parseInt(newValue, 10)
+      }
+    })
   }
 }
