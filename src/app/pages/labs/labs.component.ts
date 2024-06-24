9d3b15105b08f3b85569d67a31179d2cb62ed011
Jose Manuel <j.ochoa317@gmail.com>
2024-06-24 12:07:50 -0500

    add: example of formCotnrol in labs directory

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -19,11 +20,21 @@ export class LabsComponent {
   age = 26
   statusButton = true
   image = 'https://www.hola.com/imagenes/mascotas/20180925130054/consejos-para-cuidar-a-un-gatito-recien-nacido-cs/0-601-526/cuidardgatito-t.jpg?im=Resize=(1200)'
+  
   person = signal({
-    name: 'Jose',
+    name: 'Andrea',
     age: 22,
     avatar: 'https://josema-portfolio.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile.5e678fe0.webp&w=828&q=100'
   })
+
+  colorCtrl = new FormControl();
+
+  constructor(){
+    this.colorCtrl.valueChanges.subscribe(value => {
+      console.log(value);
+    });
+  }
+
   clickHandler(){
     alert("Alerta uno")
   }
@@ -44,6 +55,17 @@ export class LabsComponent {
       return {
         ...prevState,
         age: parseInt(newValue, 10)
+      }
+    })
+  }
+
+  changeName(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update(prevState => {
+      return {
+        ...prevState,
+        name: newValue
       }
     })
   }
